7cc9d27bcbec40add279481395508cca58f87c13
roshiwata <tatsuhiro.0323@gmail.com>
2023-11-08 00:05:01 +0900

    雛形の完成

--- seat_visual/src/App.jsx
+++ seat_visual/src/App.jsx
@@ -41,13 +41,6 @@ export const App = () => {
                 {recomText}
             </p>
 
-            <Stage width={500} height={500}>
-                <Layer>
-                    <Rect stroke='black' strokeWidth={4} x={5} y={5} width={490} height={490} />
-                    <Rect fill='red' x={100} y={100} width={300} height={200} />
-                </Layer>
-            </Stage>
-
             <Comp/>
 
         </>
--- seat_visual/src/component/comp.jsx
+++ seat_visual/src/component/comp.jsx
@@ -1,11 +1,28 @@
-import { Layer, Rect, Stage } from "react-konva";
+import { Layer, Rect, Stage, Circle } from "react-konva";
 
 export const Comp = () => {
     return (
-        <Stage width={800} height={500}>
+        <Stage width={1600} height={800}>
             <Layer>
-                <Rect stroke='black' strokeWidth={4} x={5} y={5} width={490} height={490} />
-                <Rect fill='red' x={400} y={100} width={300} height={200} />
+                <Rect stroke='black' strokeWidth={4} x={5} y={5} width={1400} height={600} />
+                <Rect fill='gray' x={100} y={100} width={300} height={150} />
+                <Rect fill='gray' x={550} y={350} width={300} height={150} />
+                <Rect fill='gray' x={1000} y={100} width={300} height={150} />
+                <Circle fill='red' x={160} y={70} radius={30} />
+                <Circle fill='red' x={330} y={70} radius={30} />
+                <Circle fill='red' x={330} y={280} radius={30} />
+                <Circle fill='red' x={160} y={280} radius={30} />
+
+                <Circle fill='red' x={620} y={320} radius={30} />
+                <Circle fill='red' x={620} y={530} radius={30} />
+                <Circle fill='red' x={790} y={320} radius={30} />
+                <Circle fill='red' x={790} y={530} radius={30} />
+
+                <Circle fill='red' x={1060} y={70} radius={30} />
+                <Circle fill='red' x={1230} y={70} radius={30} />
+                <Circle fill='red' x={1230} y={280} radius={30} />
+                <Circle fill='red' x={1060} y={280} radius={30} />
+
             </Layer>
         </Stage>
     );
